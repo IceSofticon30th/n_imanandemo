44738a736ee0b826326ef5c0f5b15bbf99f702e3
猪野 湧斗 <ice.softicon30th@gmail.com>
2015-10-04 15:23:29 +0900

    複数アカウントで運用

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # n_imanandemo
 ん？今なんでもするって言ったよね
+
+## accounts.json
+[
+  {
+    "screen_name": "n_imanandemo",
+    "consumer_key": "",
+    "consumer_secret": "",
+    "access_token": "",
+    "access_token_secret": ""
+  },
+  {
+    "screen_name": "n_imanandemo2",
+    "consumer_key": "",
+    "consumer_secret": "",
+    "access_token": "",
+    "access_token_secret": ""
+  }
+]
--- nandemo.js
+++ nandemo.js
@@ -1,14 +1,22 @@
 var Twit = require('twit');
-var KEYS = require('./keys.json');
+var ACCOUNTS = require('./accounts');
 
-var BOT_NAME = 'n_imanandemo';
+var bots = [];
+ACCOUNTS.forEach(function (ACCOUNT) {
+    bots.push(new Twit({
+        consumer_key: ACCOUNT.consumer_key,
+        consumer_secret: ACCOUNT.consumer_secret,
+        access_token: ACCOUNT.access_token,
+        access_token_secret: ACCOUNT.access_token_secret
+    }));
+});
 
-var bot = new Twit({
-    consumer_key: KEYS.consumer_key,
-    consumer_secret: KEYS.consumer_secret,
-    access_token: KEYS.access_token,
-    access_token_secret: KEYS.access_token_secret
-})
+var excludeBots = '';
+ACCOUNTS.forEach(function (ACCOUNT) {
+    excludeBots += ' -from: @' + ACCOUNT.screen_name ;
+});
+
+var botIndex = null;
 
 // 同じツイートばっかりしてるとスパム検知されるかと思って微妙に違うパターンを作っておく(効果があるとは言っていない)
 var NANDEMO = [
@@ -25,15 +33,17 @@ var repliedTo = [];
 
 // 既にリプライ済みの人のリストをリセットする時刻
 var nextUpdate;
-
 function setNextUpdate() {
     nextUpdate = new Date();
     nextUpdate.setDate(nextUpdate.getDate() + 1);
     nextUpdate.setHours(0, 0, 0, 0);
 }
+setNextUpdate();
 
 function reply(tweet) {
     console.log(tweet.user.name + '(' + tweet.user.screen_name + '): ' + tweet.text);
+    
+    var bot = bots[botIndex];
     bot.post('statuses/update',
         {
             'status': '@' + tweet.user.screen_name + ' ' + NANDEMO[Math.floor(Math.random() * NANDEMO.length)],
@@ -46,33 +56,47 @@ function reply(tweet) {
 }
 
 function judge(data) {
-    var tweets = data.statuses;
-    
     // 1日おきに既にリプライした人のリストをリセット
     if (new Date().getTime() > nextUpdate.getTime()) {
         repliedTo = [];
         setNextUpdate();
     }
     
+    var tweets = data.statuses;
+    
     // 既にリプライ済みの人を除外
     tweets.filter(function (tweet) {
         if (repliedTo.indexOf(tweet.user.id) >= 0) return false;
         else return true;
     });
-    if (tweets.length) reply(tweets[0]);
+    
+    if (tweets.length > 0) reply(tweets[0]);
 }
 
 function search() {
+    var bot = bots[botIndex];
+    
     bot.get('search/tweets',
         {
-            q: '"なんでもします" OR "なんでもする" OR "何でもする" OR "何でもします" OR "何でもやる" OR "なんでもやる" -? -？ -from:@' + BOT_NAME,
+            q: '"なんでもします" OR "なんでもする" OR "何でもする" OR "何でもします" OR "何でもやる" OR "なんでもやる" -? -？',
             result_type: 'recent',
             count: 100
         }, function (err, data, response) {
-            judge(data);
+            if (!err) judge(data);
         });
 }
 
-setNextUpdate();
-search();
-setInterval(search, 1000 * 60 * 30);
+function nextBotIndex() {
+    if (botIndex == null) {
+        botIndex = 0;
+    } else if (botIndex >= bots.length - 1) {
+        botIndex = 0;
+    } else {
+        botIndex++;
+    }
+    
+    search();
+}
+
+nextBotIndex();
+setInterval(nextBotIndex, 1000 * 60 * 30 / bots.length);
